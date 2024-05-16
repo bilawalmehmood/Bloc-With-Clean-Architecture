fe878ae4a31462fa3935d622cc45e6e42727fd92
bilwal <bilawal.mehmood@sapphire.co>
2024-05-16 15:33:26 +0500

    complete Project

---
+++ lib/src/utils/constants/strings.dart
@@ -0,0 +1,11 @@
+// App
+const String appTitle = 'Flutter Clean Architecture';
+
+// Networking and APIs
+const String baseUrl = 'https://newsapi.org/v2';
+const String defaultApiKey = 'Your-Api-Key';
+const String defaultSources = 'bbc-news, abc-news, al-jazeera-english';
+
+// Storage and Databases
+const String articlesTableName = 'articles_table';
+const String databaseName = 'app_database.db';
